eac3d60a4beb00b17438d4b7620df54051c079f8
blhoward2 <blhoward2@gmail.com>
2021-02-28 14:00:06 -0500

    fix: path

--- packages/config/maintenance/importConfig.ts
+++ packages/config/maintenance/importConfig.ts
@@ -107,7 +107,7 @@ const ozwTarUrl =
 	"https://github.com/OpenZWave/open-zwave/archive/master.tar.gz";
 const ozwConfigFolder = path.join(ozwTempDir, "./config");
 
-const zwaTempDir = path.join(__dirname, "../.tmpzwa");
+const zwaTempDir = path.join(__dirname, "../../../.tmpzwa");
 const zwaConfigFolder = path.join(zwaTempDir, "./config");
 
 const ohTempDir = path.join(__dirname, "../.tmpoh");
